d32491f5011c9ed58232515eca2ad065d88c28c2
Ifeanyi Umeh <ifeanyi4umeh@gmail.com>
2023-01-20 15:32:57 +0100

    adjusted firebase config

--- src/util/firebase.js
+++ src/util/firebase.js
@@ -27,7 +27,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+export const db = getFirestore(appCheck);
 
 // Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
 // key is the counterpart to the secret key you set in the Firebase console.
